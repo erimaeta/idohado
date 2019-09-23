3469f0245357742c982768554ccb56ea0ecd7be4
eri maeta <m226965@gmail.com>
2019-09-23 21:58:40 +0900

    いどはどの販売形態追加

--- htdocs3/assets/js/common.js
+++ htdocs3/assets/js/common.js
@@ -39,3 +39,31 @@ function setEventListener() {
     var pagetopE = document.getElementById("pagetop");
     pagetopE.addEventListener("click", goPageTop);
 }
+
+//
+// メニュー詳細へ
+//
+function goDonutsDetail(buttonNo) {
+    // var speed = 500;
+    // let buttonClassName = "goDonutsDetail";
+    // var buttons = document.getElementsByClassName(buttonClassName);
+    // var button = buttons[buttonNo];
+    // var href = button.getAttribute("href");
+}
+function arrowChange() {
+    //あとで直す
+    //		let buttonClass = "c-buttonLink";
+    //		let buttomClass = "is-arrowBottom";
+    //		var buttonE = document.getElementsByClassName(buttonClass);
+    //		buttonE.classList.contains(buttomClass);
+}
+// $(function () {
+//     $('a[href^="#"]').click(function () {
+//         var speed = 500;
+//         var href = $(this).attr("href");
+//         var target = $(href == "#" || href == "" ? 'html' : href);
+//         var position = target.offset().top;
+//         $("html, body").animate({ scrollTop: position }, speed, "swing");
+//         return false;
+//     });
+// });
