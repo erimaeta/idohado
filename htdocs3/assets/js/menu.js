99e6c1f6f1c4632014cdb8498cb97a2b5441adc3
eri maeta <m226965@gmail.com>
2020-01-03 14:44:24 +0900

    値段アップ
    >ドーナツの価格をおからプレーン10円それ以外は20円アップに変更できますか？

--- htdocs3/assets/js/menu.js
+++ htdocs3/assets/js/menu.js
@@ -4,7 +4,7 @@ var category01 = {
 	image_dir: 'assets/img/donuts/okara/',
 	image_name: '1.jpg',
 	title: 'おからプレーン',
-	price: '150',
+	price: '160',
 	description: 'いどはどドーナッツの原点、おからドーナッツ',
 	material: '小麦・卵・おから・豆乳・きび砂糖・米油・ベーキングパウダー',
 	id: 'okara1'
@@ -13,7 +13,7 @@ var category01 = {
 	image_dir: 'assets/img/donuts/okara/',
 	image_name: '2.jpg',
 	title: 'きなこ',
-	price: '160',
+	price: '180',
 	description: 'きなこ入りおからドーナッツ',
 	material: '小麦・卵・おから・豆乳・きび砂糖・米油・ベーキングパウダー・きなこ',
 	id: 'okara2'
@@ -22,7 +22,7 @@ var category01 = {
 	image_dir: 'assets/img/donuts/okara/',
 	image_name: '3.jpg',
 	title: 'ココア',
-	price: '160',
+	price: '180',
 	description: 'ココア入りおからドーナッツ',
 	material: '小麦・卵・おから・豆乳・きび砂糖・米油・ベーキングパウダー・ココア',
 	id: 'okara3'
@@ -31,7 +31,7 @@ var category01 = {
 	image_dir: 'assets/img/donuts/okara/',
 	image_name: '4.jpg',
 	title: '抹茶',
-	price: '170',
+	price: '190',
 	description: '抹茶入りおからドーナッツ',
 	material: '小麦・卵・おから・豆乳・きび砂糖・米油・ベーキングパウダー・抹茶',
 	id: 'okara4'
@@ -40,7 +40,7 @@ var category01 = {
 	image_dir: 'assets/img/donuts/okara/',
 	image_name: '5.jpg',
 	title: '白い誘惑',
-	price: '180',
+	price: '200',
 	description: 'ホワイトチョコで厚化粧。この白い穴へとあなたをいざなう？',
 	material: '小麦・卵・おから・豆乳・きび砂糖・米油・ベーキングパウダー・ホワイトチョコレート・ラズベリーチップ',
 	id: 'okara5'
@@ -49,7 +49,7 @@ var category01 = {
 	image_dir: 'assets/img/donuts/okara/',
 	image_name: '6.jpg',
 	title: '不安定ボーイ',
-	price: '180',
+	price: '200',
 	description: 'チョコクランチの前髪パッツンの男の子',
 	material: '小麦・卵・おから・豆乳・きび砂糖・米油・ベーキングパウダー・きな粉・チョコクランチ・アーモンドスライス・ミルクチョコレート',
 	id: 'okara6'
@@ -58,7 +58,7 @@ var category01 = {
 	image_dir: 'assets/img/donuts/okara/',
 	image_name: '7.jpg',
 	title: 'ブラックホール',
-	price: '180',
+	price: '200',
 	description: 'ミルクチョコレートでコーティング。宇宙の闇からこんばんわー',
 	material: '小麦・卵・おから・豆乳・きび砂糖・米油・ベーキングパウダー・ココア・ミルクチョコレート・アラザン',
 	id: 'okara7'
@@ -67,7 +67,7 @@ var category01 = {
 //	image_dir: 'assets/img/donuts/okara/',
 //	image_name: '8.jpg',
 //	title: 'ココシャネル',
-//	price: '180',
+//	price: '200',
 //	description: 'ココナッツ・ファーをまとったおしゃれなご婦人',
 //	material: '小麦・卵・おから・豆乳・きび砂糖・米油・ベーキングパウダー・ココア・ココナッツ・ココナッツミルク・粉糖',
 //	id: 'okara8'
@@ -76,7 +76,7 @@ var category01 = {
 	image_dir: 'assets/img/donuts/okara/',
 	image_name: '9.jpg',
 	title: 'ブラックドラゴン',
-	price: '180',
+	price: '200',
 	description: 'いちじく入りココアドーナッツ。目が合うとたいへん！とりこになる恐れあり。',
 	material: '小麦・卵・おから・豆乳・きび砂糖・米油・ベーキングパウダー・ココア・いちじく・チョコチップ',
 	id: 'okara9'
@@ -85,7 +85,7 @@ var category01 = {
 	image_dir: 'assets/img/donuts/okara/',
 	image_name: '10.jpg',
 	title: '新星ゴリョウ',
-	price: '200',
+	price: '220',
 	description: '抹茶チョコでコーティング。五芒星は当然、五稜郭。',
 	material: '小麦・卵・おから・豆乳・きび砂糖・米油・ベーキングパウダー・抹茶・ホワイトチョコレート',
 	id: 'okara10'
@@ -94,7 +94,7 @@ var category01 = {
 	image_dir: 'assets/img/donuts/okara/',
 	image_name: '11.jpg',
 	title: 'ハドウィーン',
-	price: '180',
+	price: '200',
 	description: 'かぼちゃたっぷりの波動を送ります',
 	material: '小麦・卵・おから・豆乳・きび砂糖・米油・ベーキングパウダー・かぼちゃ',
 	id: 'okara11'
@@ -108,7 +108,7 @@ var category02 = {
 	image_dir: 'assets/img/donuts/butter/',
 	image_name: '1.jpg',
 	title: 'アバター',
-	price: '180',
+	price: '200',
 	description: 'あっ！バター？神の化身にして、おのれの分身、アバターは、お菓子の国で大暴れ！',
 	material: '小麦・卵・アーモンドパウダー・牛乳・きび砂糖・バター・ベーキングパウダー',
 	id: 'butter1'
@@ -117,7 +117,7 @@ var category02 = {
 	image_dir: 'assets/img/donuts/butter/',
 	image_name: '2.jpg',
 	title: 'ギャラクシー',
-	price: '200',
+	price: '220',
 	description: '銀河＆星雲・・・宇宙の果てに吸い込まれる時に見える情景がコレ！たぶん・・・',
 	material: '小麦・卵・アーモンドパウダー・牛乳・きび砂糖・バター・ベーキングパウダー・粉糖・赤色・青色',
 	id: 'butter2'
@@ -126,7 +126,7 @@ var category02 = {
 	image_dir: 'assets/img/donuts/butter/',
 	image_name: '3.jpg',
 	title: 'シナモンスター',
-	price: '200',
+	price: '220',
 	description: 'シナモン星からやってきたシナモンスター。',
 	material: '小麦・卵・アーモンドパウダー・牛乳・きび砂糖・バター・ベーキングパウダー・シナモン・くるみ',
 	id: 'butter3'
@@ -135,7 +135,7 @@ var category02 = {
 	image_dir: 'assets/img/donuts/butter/',
 	image_name: '4.jpg',
 	title: 'ムーンマトリックス',
-	price: '200',
+	price: '220',
 	description: 'レモンのアイシングがまるで月の様。アポロ11号の宇宙食',
 	material: '小麦・卵・アーモンドパウダー・牛乳・きび砂糖・バター・ベーキングパウダー・粉糖・レモン果汁・黄色',
 	id: 'butter4'
@@ -144,7 +144,7 @@ var category02 = {
 	image_dir: 'assets/img/donuts/butter/',
 	image_name: '5.jpg',
 	title: 'やよい',
-	price: '200',
+	price: '220',
 	description: '春限定の桜色の和風ドーナッツ',
 	material: '小麦・卵・アーモンドパウダー・牛乳・きび砂糖・バター・ベーキングパウダー・粉糖・粒あん・桜パウダー・ピンク色',
 	id: 'butter5'
@@ -158,7 +158,7 @@ var category03 = {
 	image_dir: 'assets/img/donuts/noegg/',
 	image_name: '1.jpg',
 	title: 'ナイシローン (くるみ入り)',
-	price: '180',
+	price: '200',
 	description: '卵が苦手な方もそうでない方も好きになる、卵無しのおからドーナッツ',
 	material: '小麦（全粒粉）・アーモンドパウダー・きび砂糖・米油・ベーキングパウダー・くるみ',
 	id: 'noegg1'
@@ -167,7 +167,7 @@ var category03 = {
 	image_dir: 'assets/img/donuts/noegg/',
 	image_name: '2.jpg',
 	title: 'ナイシローン (チョコ)',
-	price: '180',
+	price: '200',
 	description: '卵無しおからドーナッツにチョコチップが入って、より美味しい',
 	material: '小麦（全粒粉）・アーモンドパウダー・きび砂糖・米油・ベーキングパウダー・チョコチップ',
 	id: 'noegg2'
@@ -176,7 +176,7 @@ var category03 = {
 	image_dir: 'assets/img/donuts/noegg/',
 	image_name: '3.jpg',
 	title: 'ナイクローン (くるみ入り)',
-	price: '180',
+	price: '200',
 	description: '卵無しの黒い奴。別名、クローン。',
 	material: '小麦（全粒粉）・アーモンドパウダー・きび砂糖・米油・ベーキングパウダー・くるみ・ココア',
 	id: 'noegg3'
