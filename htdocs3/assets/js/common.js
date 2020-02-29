d4144169a6a90ae50cc8ce68ffd959a7e8588a06
eri maeta <m226965@gmail.com>
2020-02-29 14:24:45 +0900

    スムーススクロール導入

--- htdocs3/assets/js/common.js
+++ htdocs3/assets/js/common.js
@@ -40,6 +40,25 @@ function setEventListener() {
     pagetopE.addEventListener("click", goPageTop);
 }
 
+
+$( function(){
+  //
+  // スムーススクロール
+  //
+  $('a[href^="#"]').click(
+    function(){
+      var adjust = 0;
+      var speed = 400;
+      var href = $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top + adjust;
+      $('body,html').animate( {scrollTop:position}, speed, 'swing');
+      return false;
+    }
+  );
+});
+
+
 //
 // メニュー詳細へ
 //
