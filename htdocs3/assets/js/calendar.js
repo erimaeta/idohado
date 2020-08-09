ed219253ddbd23de66cf790723953985069ee6b3
eri maeta <maeta@hakoreco.com>
2020-08-10 00:27:00 +0900

    8月の更新と、クール便のsection追加

--- htdocs3/assets/js/calendar.js
+++ htdocs3/assets/js/calendar.js
@@ -8,7 +8,7 @@
 //営業日と休業日を配列で指定
 var eigyoArray = [0,1,2,4,5,6]; //営業日: 日0 月1 火2 水3 木4 金5 土6
 //var eigyoArray = [1,2,5,6]; //営業日: 日0 月1 火2 水3 木4 金5 土6
-var kyugyoArray = [1,2,3,4,5,6,7]; //休業日
+var kyugyoArray = []; //休業日
 
 //クラス名
 var table_ClassName = 'calendar';
@@ -19,7 +19,7 @@ function show_calendar() {
     var year = current.getFullYear();
     var month = current.getMonth() + 1;
 
-    month =7;
+    month =8;
     //console.log(month);
 
     var wrapper = document.getElementById('calendar');
